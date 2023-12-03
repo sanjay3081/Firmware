b22d077109be3029562e7812e3cdc4a6b91bc40a
Sanjay Prasanth A S <84510522+sanjay3081@users.noreply.github.com>
2023-12-03 18:48:07 +0530

    Update mcu_to_pc.c

--- mcu_to_pc.c
+++ mcu_to_pc.c
@@ -1,90 +1,93 @@
 #include <lpc214x.h>
-#include <string.h>
 
-#define DATA_SIZE 1000
+#define BAUD_RATE 2400
+#define CRC16_POLY 0xA001
+#define EEPROM_START_ADDRESS 0x0000
 
-unsigned short calculate_crc(char *data, int length) {
-    unsigned short crc = 0;
-    for (int i = 0; i < length; i++) {
-        crc ^= (unsigned short)data[i] << 8;
-        for (int j = 0; j < 8; j++) {
-            if (crc & 0x8000)
-                crc = (crc << 1) ^ 0x1021;
-            else
-                crc <<= 1;
+void initUART() {
+    U0LCR = 0x03;
+    U0LCR |= (1 << 7);
+    U0DLL = (PCLK / (16 * BAUD_RATE)) % 256;
+    U0DLM = (PCLK / (16 * BAUD_RATE)) / 256;
+    U0LCR &= ~(1 << 7);
+    U0FCR = 0x07;
+}
+
+void uartSend(char data) {
+    while (!(U0LSR & (1 << 5)));
+    U0THR = data;
+}
+
+char uartReceive() {
+    while (!(U0LSR & 0x01));
+    return U0RBR;
+}
+
+void eepromWrite(unsigned int address, char data) {
+    IAP_CONSET = 0x01;
+    IAP_ADDRH = (address >> 8) & 0xFF;
+    IAP_ADDRL = address & 0xFF;
+    IAP_CMD = 61;
+    IAP_DATA = data;
+    IAP_CONSET = 0x04;
+    IAP_TRIG = 0x46;
+    IAP_TRIG = 0xB9;
+    while (IAP_CONSET & 0x04);
+    IAP_CONCLR = 0x0C;
+}
+
+char eepromRead(unsigned int address) {
+    IAP_CMD = 62;
+    IAP_ADDRH = (address >> 8) & 0xFF;
+    IAP_ADDRL = address & 0xFF;
+    IAP_TRIG = 0x46;
+    IAP_TRIG = 0xB9;
+    while (IAP_TRIG & 0x01);
+    return IAP_DATA;
+}
+
+uint16_t calculateCRC16(char *data, size_t length) {
+    uint16_t crc = 0xFFFF;
+
+    while (length--) {
+        crc ^= *data++;
+
+        for (int i = 0; i < 8; i++) {
+            if (crc & 0x0001) {
+                crc = (crc >> 1) ^ CRC16_POLY;
+            } else {
+                crc >>= 1;
+            }
         }
     }
+
     return crc;
-}
-
-void init_uart() {
-    PINSEL0 |= (1 << 4) | (1 << 6); 
-    U0LCR = 0x83; 
-    U0DLL = 78;   
-    U0LCR = 0x03; 
-}
-
-void send_char(char ch) {
-    while (!(U0LSR & 0x20)); 
-    U0THR = ch; 
-}
-
-char read_char() {
-    while (!(U0LSR & 0x01)); 
-    return U0RBR; 
-}
-
-void send_crc(unsigned short crc) {
-    send_char((char)(crc >> 8));
-    send_char((char)crc);
-}
-
-unsigned short read_crc() {
-    unsigned short crc = (read_char() << 8);
-    crc |= read_char();
-    return crc;
-}
-
-void receive_data_from_pc() {
-    char received_text[DATA_SIZE];
-    unsigned short received_crc;
-    int data_index = 0;
-
-    init_uart();
-
-    while (data_index < DATA_SIZE) {
-        char received_char = read_char();
-        unsigned short received_char_crc = read_crc();
-        unsigned short calculated_crc = calculate_crc(&received_char, 1);
-
-        if (calculated_crc == received_char_crc) {
-            received_text[data_index++] = received_char;
-        } else {
-        }
-    }
-
-    for (int i = 0; i < DATA_SIZE; i++) {
-        EEPROM_write(i, received_text[i]);
-    }
-}
-
-void send_data_to_pc() {
-    char stored_data[DATA_SIZE];
-
-    for (int i = 0; i < DATA_SIZE; i++) {
-        stored_data[i] = EEPROM_read(i);
-    }
-
-    // Send the stored data to PC via UART
-    for (int i = 0; i < DATA_SIZE; i++) {
-        send_char(stored_data[i]);
-        send_crc(calculate_crc(&stored_data[i], 1));
-    }
 }
 
 int main() {
-    receive_data_from_pc();
-    send_data_to_pc();
+    initUART();
+
+    char buffer[256];
+    size_t bufferIndex = 0;
+
+    while (1) {
+        char receivedByte = uartReceive();
+        if (receivedByte == '\0') {
+            break;
+        }
+        
+        eepromWrite(EEPROM_START_ADDRESS + bufferIndex, receivedByte);
+        bufferIndex++;
+
+        if (bufferIndex == sizeof(buffer)) {
+            break;
+        }
+    }
+
+    for (size_t i = 0; i < bufferIndex; i++) {
+        char eepromData = eepromRead(EEPROM_START_ADDRESS + i);
+        uartSend(eepromData);
+    }
 
     return 0;
 }
